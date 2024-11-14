e1c288c10c48ebad52cb110c4beb512f0dc46d1e
sdery5 <sheiradery@gmail.com>
2024-11-14 11:27:50 -0500

    Add files via upload

---
+++ lab5q19.py
@@ -0,0 +1,31 @@
+#question 19
+
+
+import random 
+ 
+def guess_the_number(): 
+    
+    number_to_guess = random.randint(1, 100) 
+    guess = None 
+ 
+    print("Welcome to the Guess the Number game!") 
+    print("I have selected a number between 1 and 100. Can you guess it?") 
+ 
+
+    while guess != number_to_guess: 
+        try: 
+            
+            guess = int(input("Enter your guess: ")) 
+             
+             
+            if guess < number_to_guess: 
+                print("Too low! Try again.") 
+            elif guess > number_to_guess: 
+                print("Too high! Try again.") 
+            else: 
+                print("Congratulations! You've guessed the number!") 
+        except ValueError: 
+            print("Please enter a valid integer.") 
+ 
+ 
+guess_the_number() 
